3009b0a188b21fe2fe587d6b02f83eafd9ec78df
hll_RGB <hll_RGB@outlook.com>
2026-03-22 13:49:00 +0800

    Delete src/nrl/neoreadline.c

--- src/nrl/neoreadline.c
+++
@@ -1,8 +0,0 @@
-#include "neoreadline.h"
-#include <termios.h>
-struct termios cmdline_termios_attr readline_termios_attr;
-uint32_t nrl_point;  // 当前字符
-uint32_t nrl_line;   // 当前行
-uint32_t nrl_length; // 所在行长度
-uint32_t void nrl_init(void) {}
-char *neoreadline(const char *prompt) {}
